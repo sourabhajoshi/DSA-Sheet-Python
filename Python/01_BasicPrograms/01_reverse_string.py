8f78e05eba0b40632adb23a9b98933a80550776a
Joshi Sourabha <joshisourabha@Joshis-MacBook-Pro.local>
2025-07-26 21:50:37 +0530

    Reverse string

--- Python/01_BasicPrograms/01_reverse_string.py
+++ Python/01_BasicPrograms/01_reverse_string.py
@@ -1,1 +1,11 @@
-# Hello 
+# Write a program that takes a string input from the user and prints the reversed string.
+
+def reverse_string(text):
+  reverse_text = ""
+  for char in text:
+    reverse_text = char + reverse_text
+  return reverse_text
+    
+user_input = input('Enter a string : ')
+print(f"Revered string is : {reverse_string(user_input)}")
+
